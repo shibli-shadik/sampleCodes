a75ab4473033c6f6adacfea95b86199bdc62ba88
shibli <shibli.shadik@gmail.com>
2019-12-19 12:15:22 +0600

    initial commit

---
+++ src/main/java/codes/SampleCodesApplication.java
@@ -0,0 +1,13 @@
+package codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCodesApplication.class, args);
+	}
+
+}
